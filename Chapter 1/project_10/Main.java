b41ace9e7139b55cf52b803e816169ee293445bb
Maksim Petrosyan <maxsim.petrosyan@gmail.com>
2024-07-10 17:32:52 +0400

    The solution of project 10 is added

---
+++ Chapter 1/project_10/Main.java
@@ -0,0 +1,20 @@
+/**
+ * Scientists estimate that roughly 10 grams of caffeine consumed at one time is a
+ * lethal overdose. Write a program with a variable that holds the number of milligrams of
+ * caffeine in a drink and outputs how many drinks it takes to kill a person.
+ * A 12-ounce can of cola has approximately 34 mg of caffeine, while a 16-ounce cup
+ * of coffee has approximately 160 mg of caffeine.
+ *
+ * @author Maksim Petrosyan
+ * */
+public class Main {
+    public static final int LETHAL_OVERDOSE = 10000;
+    public static final int CAFFEINE_IN_COLA = 34;
+    public static final int CAFFEINE_IN_COFFEE = 160;
+    public static void main(String[] args) {
+        int lethalNumOfCola = LETHAL_OVERDOSE / CAFFEINE_IN_COLA + 1;
+        int lethalNumOfCoffee = LETHAL_OVERDOSE / CAFFEINE_IN_COFFEE + 1;
+        System.out.println("Lethal number of cola cans: " + lethalNumOfCola);
+        System.out.println("Lethal number of coffee cups: " + lethalNumOfCoffee);
+    }
+}
