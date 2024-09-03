ae6833266d489f6c3eab07bd4db54ad3b170535a
Maksim Petrosyan <maxsim.petrosyan@gmail.com>
2024-09-03 16:12:39 +0400

    JavaDoc comments of problem 2 was added

--- Chapter 5/project_02/Fraction.java
+++ Chapter 5/project_02/Fraction.java
@@ -1,37 +1,82 @@
+/**
+ * Represents a fraction with a numerator and a denominator.
+ *
+ * @author Maksim Petrosyan
+ * */
 public class Fraction {
     private int numerator;
     private int denominator;
 
+    /**
+     * Constructs a fraction with the given numerator and denominator
+     *
+     * @param numerator numerator of the fraction
+     * @param denominator denominator of the fraction
+     * */
     public Fraction(int numerator, int denominator) {
         this.numerator = numerator;
         this.denominator = denominator;
     }
 
+    /**
+     * Constructs a fraction with the same numerator and denominator as another fraction
+     *
+     * @param other another fraction
+     * */
     public Fraction(Fraction other){
         this.numerator = other.getNumerator();
         this.denominator = other.getDenominator();
     }
 
+    /**
+     * sets the numerator of the fraction
+     *
+     * @param numerator the new numerator value
+     * */
     public void setNumerator(int numerator){
         this.numerator = numerator;
     }
 
+    /**
+     * sets the denominator of the fraction
+     *
+     * @param denominator the new denominator value
+     * */
     public void setDenominator(int denominator){
         this.denominator = denominator;
     }
 
+    /**
+     * Gets the numerator of the fraction
+     *
+     * @return the numerator of the fraction
+     * */
     public int getNumerator(){
         return numerator;
     }
 
+    /**
+     * Gets the denominator of the fraction
+     *
+     * @return the denominator of the fraction
+     * */
     public int getDenominator(){
         return denominator;
     }
 
+    /**
+     * Returns a string representation of the fraction in the form "numerator/denominator".
+     *
+     * @return a string representation of the fraction
+     */
     public String toString(){
         return this.numerator + "/" + this.denominator;
     }
 
+    /**
+     * Reduces the fraction to its lowest terms by dividing the numerator and denominator
+     * by their greatest common divisor.
+     */
     private void toLowestTerms(){
         int i = 2;
         while(i <= this.numerator){
@@ -44,9 +89,20 @@ public class Fraction {
         }
     }
 
+    /**
+     * Checks if this fraction is equal to another fraction. Two fractions are considered
+     * equal if they have the same numerator and denominator when both are reduced to their
+     * lowest terms.
+     *
+     * @param other the fraction to compare to this fraction
+     * @return true if the fractions are equal, false otherwise
+     */
     public boolean equals(Fraction other){
         Fraction temp = new Fraction(other);
+        Fraction tempForThis = new Fraction(this);
         temp.toLowestTerms();
-        return (this.numerator == temp.getNumerator() && this.denominator == temp.getDenominator());
+        tempForThis.toLowestTerms();
+        return (tempForThis.getNumerator() == temp.getNumerator() &&
+                tempForThis.getDenominator() == temp.getDenominator());
     }
 }
