47af173b84ebe95ac609f062be88992e12e12600
Maksim <maxsim.petrosyan@gmail.com>
2024-12-29 13:14:45 +0400

    Added solution for problem 9 in chapter 6

--- Chapter 6/project_09/Main.java
+++ Chapter 6/project_09/Main.java
@@ -1,3 +1,17 @@
+/**
+ * Enhance the definition of the class PartiallyFilledArray (Display 6.5) in the
+ * following way: When the user attempts to add one additional element and there
+ * is no room in the array instance variable a, the user is allowed to add the element.
+ * The object creates a second array that is twice the size of the array a, copies values
+ * from the array a to the user’s new array, makes this array (or more precisely its
+ * reference) the new value of a, and then adds the element to this new larger array a.
+ * Hence, this new class should have no limit (other than the physical size of the computer) to how many numbers it can hold. The instance variable maxNumberOfElements
+ * remains and the method getMaxCapacity is unchanged, but these now refer to the
+ * currently allocated memory and not to an absolute upper bound. Write a suitable
+ * test program.
+ *
+ * @author Maksim Petrosyan
+ * */
 public class Main {
     public static void main(String[] args) {
         PartiallyFilledArray parr = new PartiallyFilledArray(2);
