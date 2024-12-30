b64e6defd812afb705604e57cf10731a91c3d323
Maksim <maxsim.petrosyan@gmail.com>
2024-12-30 13:01:53 +0400

    Added solution for problem 18 in chapter 6

--- Chapter 6/project_18/Main.java
+++ Chapter 6/project_18/Main.java
@@ -1,7 +1,17 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
+/**
+ * Programming Project 4.13 asked you to create a BoxOfProduce class representing
+ * a box of produce to deliver from a CSA farm. The box contained exactly three
+ * items. Modify the class so it uses an array of type String to represent the items
+ * in the box. You can still start with three random items to place in the box, but
+ * your menu should be modified to allow the user to add additional items and still
+ * substitute one item for another. You will likely need to modify the constructor of
+ * the BoxOfProduce class and also add new methods.
+ *
+ * @author Maksim Petrosyan
+ * */
 public class Main {
     public static void main(String[] args) {
         Scanner sc = null;
